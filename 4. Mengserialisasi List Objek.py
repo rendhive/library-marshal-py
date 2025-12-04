e2dc40c553729d956dddf5c00d50df675358788d
rendhive <rendhivecode@gmail.com>
2025-12-04 13:32:31 +0700

    [DOCT] Sample Library Python:4. Mengserialisasi List Objek.py

---
+++ 4. Mengserialisasi List Objek.py
@@ -0,0 +1,10 @@
+import marshal
+
+data_list = [1, 2, 3, {'a': 'A', 'b': 'B'}]
+
+with open('list_data.marshal', 'wb') as file:
+    marshal.dump(data_list, file)
+
+print("List berhasil diserialisasi dan disimpan ke 'list_data.marshal'.")
+# Fungsi: Menyimpan list (atau koleksi) objek Python ke dalam file.
+# Kondisi: Ketika Anda ingin menyimpan koleksi objek untuk diproses kemudian.
