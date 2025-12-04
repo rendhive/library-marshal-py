289fcffccbf90aa57f2535351ad7e54292672ba5
rendhive <rendhivecode@gmail.com>
2025-12-04 13:31:33 +0700

    [DOCT] Sample Library Python: 1. Mengimpor Modul marshal.py

---
+++ 1. Mengimpor Modul marshal.py
@@ -0,0 +1,5 @@
+import marshal
+
+print("Modul marshal berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk melakukan serialisasi dan deserialisasi objek Python.
+# Kondisi: Digunakan sebelum memanfaatkan fungsionalitas dari marshal.
